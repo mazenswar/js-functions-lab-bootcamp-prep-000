6915f2dd4cf8c242f0e66821aceb9ca7336ec2b0
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-08 18:52:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}`;
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday, name) {
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, numOfDays) {
-  return `It's ${numOfDays} days until ${holiday}`;
+  return `It's ${numOfDays} days until ${holiday}!`;
 }
